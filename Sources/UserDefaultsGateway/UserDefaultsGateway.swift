efd3378a0bacf3821c0e05d1d1669200e3a7e6d1
Andrei Krotov <akrotoff95@gmail.com>
2023-09-12 15:48:26 +0300

    Added package and its tests

--- Sources/UserDefaultsGateway/UserDefaultsGateway.swift
+++ Sources/UserDefaultsGateway/UserDefaultsGateway.swift
@@ -1,6 +1,50 @@
-public struct UserDefaultsGateway {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public protocol UserDefaultsGatewayType {
+    func setObject<ObjectType: Encodable>(_ object: ObjectType, forKey key: String) throws
+    func object<ObjectType: Decodable>(forKey key: String) throws -> ObjectType?
+    func removeObject(forKey key: String)
+}
+
+public final class UserDefaultsGateway {
+
+    // MARK: - Private properties
+
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    private let defaults = UserDefaults.standard
+
+    // MARK: - Internal methods
+}
+
+extension UserDefaultsGateway: UserDefaultsGatewayType {
+
+    public func setObject<ObjectType: Encodable>(_ object: ObjectType, forKey key: String) throws {
+        if object is String || object is Float || object is Bool || object is Double || object is Int {
+            defaults.set(object, forKey: key)
+        } else {
+            let data = try encoder.encode(object)
+            defaults.set(data, forKey: key)
+        }
+    }
+
+    public func object<ObjectType: Decodable>(forKey key: String) throws -> ObjectType? {
+        if
+            ObjectType.self == String.self
+                || ObjectType.self == Float.self
+                || ObjectType.self == Bool.self
+                || ObjectType.self == Double.self
+                || ObjectType.self == Int.self
+        {
+            return defaults.object(forKey: key) as? ObjectType
+        } else if let data = defaults.data(forKey: key) {
+            return try decoder.decode(ObjectType.self, from: data)
+        }
+
+        return nil
+    }
+
+    public func removeObject(forKey key: String) {
+        defaults.removeObject(forKey: key)
     }
 }
--- Tests/UserDefaultsGatewayTests/UserDefaultsGatewayTests.swift
+++ Tests/UserDefaultsGatewayTests/UserDefaultsGatewayTests.swift
@@ -2,10 +2,126 @@ import XCTest
 @testable import UserDefaultsGateway
 
 final class UserDefaultsGatewayTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UserDefaultsGateway().text, "Hello, World!")
+
+    var gateway: UserDefaultsGatewayType!
+
+    override func setUp() {
+        gateway = UserDefaultsGateway()
+    }
+
+    func test_ifSetString_thenThereIsTheSameValue() {
+        setAndCheck("Test_string")
+    }
+
+    func test_ifSetFloat_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setAndCheck(Float.random(in: -1000...1000))
+        }
+    }
+
+    func test_ifSetBool_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setAndCheck(Bool.random())
+        }
+    }
+
+    func test_ifSetDouble_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setAndCheck(Double.random(in: -1000...1000))
+        }
+    }
+
+    func test_ifSetInt_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setAndCheck(Int.random(in: -1000...1000))
+        }
+    }
+
+    func test_ifSetCustomObject_thenThereIsTheSameValue() {
+        struct Object: Codable, Equatable {
+            let int: Int
+            let string: String
+            let float: Float
+        }
+
+        for _ in 0..<10 {
+            setAndCheck(Object(
+                int: Int.random(in: -1000...1000),
+                string: "Test_string",
+                float: Float.random(in: -1000...1000)
+            ))
+        }
+    }
+
+    func test_ifSetStringAndRemove_thenThereIsTheSameValue() {
+        setRemoveAndCheck("Test_string")
+    }
+
+    func test_ifSetFloatAndRemove_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setRemoveAndCheck(Float.random(in: -1000...1000))
+        }
+    }
+
+    func test_ifSetBoolAndRemove_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setAndCheck(Bool.random())
+        }
+    }
+
+    func test_ifSetDoubleAndRemove_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setRemoveAndCheck(Double.random(in: -1000...1000))
+        }
+    }
+
+    func test_ifSetIntAndRemove_thenThereIsTheSameValue() {
+        for _ in 0..<10 {
+            setRemoveAndCheck(Int.random(in: -1000...1000))
+        }
+    }
+
+    func test_ifSetCustomObjectAndRemove_thenThereIsTheSameValue() {
+        struct Object: Codable, Equatable {
+            let int: Int
+            let string: String
+            let float: Float
+        }
+
+        for _ in 0..<10 {
+            setRemoveAndCheck(Object(
+                int: Int.random(in: -1000...1000),
+                string: "Test_string",
+                float: Float.random(in: -1000...1000)
+            ))
+        }
+    }
+
+    private func setAndCheck<ObjectType: Codable & Equatable>(_ value: ObjectType) {
+        let key = "some.test.key"
+
+        do {
+            try gateway.setObject(value, forKey: key)
+
+            let result: ObjectType? = try gateway.object(forKey: key)
+
+            XCTAssertEqual(value, result)
+        } catch {
+            XCTFail("Test failed with caught error \(error)")
+        }
+    }
+
+    private func setRemoveAndCheck<ObjectType: Codable & Equatable>(_ value: ObjectType) {
+        let key = "some.test.key"
+
+        do {
+            try gateway.setObject(value, forKey: key)
+            gateway.removeObject(forKey: key)
+            let result: ObjectType? = try gateway.object(forKey: key)
+
+            XCTAssertEqual(nil, result)
+        } catch {
+            XCTFail("Test failed with caught error \(error)")
+        }
     }
 }
